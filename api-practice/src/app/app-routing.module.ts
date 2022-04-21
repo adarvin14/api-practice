4aaf7f7e371af847653be500854983455a2b7434
adarvin14 <amdarvin14@gmail.com>
2022-04-21 09:01:16 -0400

    delete placeholder app html

--- api-practice/src/app/app-routing.module.ts
+++ api-practice/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuoteComponent } from './components/quote/quote.component';
+import { SearchComponent } from './components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: QuoteComponent
+  },{
+    path: '',
+    component: SearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- api-practice/src/app/app.module.ts
+++ api-practice/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './components/quote/quote.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
